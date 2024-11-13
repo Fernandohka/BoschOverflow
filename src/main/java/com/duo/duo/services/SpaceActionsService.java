c5ee17e9c54090bd6207627b98f1ddf278e3e818
akauzinha47 <kau.vmenezes@gmail.com>
2024-11-13 09:59:20 -0300

    space interface functions return change

--- src/main/java/com/duo/duo/services/SpaceActionsService.java
+++ src/main/java/com/duo/duo/services/SpaceActionsService.java
@@ -1,11 +1,13 @@
 package com.duo.duo.services;
 
+import org.springframework.http.ResponseEntity;
+
 import com.duo.duo.model.Space;
 import com.duo.duo.model.UserSpace;
 
-public interface SpaceActionsService {
-    public void postSpace(Space space);                     //Criar um novo espaço
-    public void addUser(UserSpace userSpace);               //Adicionar usuário ao espaço
-    public void patchPermission(UserSpace userSpace);       //Alterar o nível de permissão de um usuário do espaço
-    public void deleteSpace(Space space);                   //Apagar espaço
+public interface SpaceActionsService<T> {
+    public ResponseEntity<T> postSpace(Space space);                     //Criar um novo espaço
+    public ResponseEntity<T> addUser(UserSpace userSpace);               //Adicionar usuário ao espaço
+    public ResponseEntity<T> patchPermission(UserSpace userSpace);       //Alterar o nível de permissão de um usuário do espaço
+    public ResponseEntity<T> deleteSpace(Space space);                   //Apagar espaço
 }
--- src/main/java/com/duo/duo/services/implementations/SpaceActionImplementation.java
+++ src/main/java/com/duo/duo/services/implementations/SpaceActionImplementation.java
@@ -1,33 +1,36 @@
 package com.duo.duo.services.implementations;
 
+import org.springframework.http.ResponseEntity;
+
 import com.duo.duo.model.Space;
 import com.duo.duo.model.UserSpace;
 import com.duo.duo.services.SpaceActionsService;
 
-public class SpaceActionImplementation implements SpaceActionsService{
+public class SpaceActionImplementation implements SpaceActionsService<Space>{
 
     @Override
-    public void postSpace(Space space) {
+    public ResponseEntity<Space> postSpace(Space space) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'postSpace'");
     }
 
     @Override
-    public void addUser(UserSpace userSpace) {
+    public ResponseEntity<Space> addUser(UserSpace userSpace) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'addUser'");
     }
 
     @Override
-    public void patchPermission(UserSpace userSpace) {
+    public ResponseEntity<Space> patchPermission(UserSpace userSpace) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'patchPermission'");
     }
 
     @Override
-    public void deleteSpace(Space space) {
+    public ResponseEntity<Space> deleteSpace(Space space) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'deleteSpace'");
     }
+
     
 }
