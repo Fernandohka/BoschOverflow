5823b97d792c612f151a96b1239005fef7d2bb78
ingridcriistine <ingridrochacristine@gmail.com>
2024-11-22 09:42:03 -0300

    login

--- front/assets/script/login.js
+++ front/assets/script/login.js
@@ -1,11 +1,24 @@
 async function  login(){
     let login = document.getElementById("login").value;
     let pass = document.getElementById("pass").value;
+
     console.log(login);
     console.log(pass);
-    let res = await fetch("http://localhost:8080/auth",{
+
+    let res = await fetch("http://localhost:8080/auth",
+    {
         method:"POST",
+        headers:{"Content-Type":"application/json"},
         body:JSON.stringify({"login":login,"password":pass})
+    }).then((res)=>{
+        res.json().then(data =>  {
+            console.log(data)
+            alert(data.messages)
+
+            if(data.token != null) {
+                sessionStorage.setItem("token", data.token);
+                window.location.href = "http://127.0.0.1:5500/front/home.html"
+            }
+        })
     });
-    console.log(res);
 }
