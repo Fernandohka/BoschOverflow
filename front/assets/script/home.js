59a8865555a48e94785c5999ccf4adc410664868
AmiltonMn <amilton.moreiran@hotmail.com>
2024-11-25 08:26:01 -0300

    AAA

--- front/assets/script/home.js
+++ front/assets/script/home.js
@@ -1,26 +1,27 @@
-async function loadSpaces(){
-    console.log("Bearer "+window.sessionStorage.getItem("token"))
-    await fetch("http://localhost:8080/space/get?query=&page=0&limit=5",{
-        method:"GET",
-        headers:{"Content-Type":  "application/json","Authorization":"Bearer "+window.sessionStorage.getItem("token")}
-    }).then((res)=>{
-        res.json().then(data=>{
-            console.log(data)
+async function loadSpaces() {
+    console.log("Bearer " + window.sessionStorage.getItem("token"))
+    await fetch("http://localhost:8080/space/get?query=&page=0&limit=5", {
+        method: "GET",
+        headers: { "Content-Type": "application/json", "Authorization": "Bearer " + window.sessionStorage.getItem("token") }
+    }).then((res) => {
+        if (res.status==403){
+            window.location.replace("http://127.0.0.1:5500/login.html");
+        }
+        res.json().then(data => {
             let boxSpace = document.getElementById("divSpaces")
-            console.log(boxSpace)
             data.forEach(space => {
-            let a = document.createElement("a");
-            a.className = "btn btn-dark btn-space";
-            a.onclick = `toSpace(${space.id})`;
-            a.text = space.name;
-            boxSpace.appendChild(a);
-    });
+                let a = document.createElement("a");
+                a.className = "btn btn-dark btn-space";
+                a.onclick = `toSpace(${space.id})`;
+                a.text = space.name;
+                boxSpace.appendChild(a);
+            });
         })
     });
 }
 
-async function toSpace(id){
-    window.localStorage.setItem("spaceId",id);
+async function toSpace(id) {
+    window.localStorage.setItem("spaceId", id);
     window.location.href = "http://127.0.0.1:5500/spaces.html"
 }
 
